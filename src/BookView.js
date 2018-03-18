6ab3adfbde96356ad1b78f019c9427e81615e884
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-18 11:20:06 -0300

    Creating BookView component

---
+++ src/BookView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BookView extends Component {
+    render() {
+        return (
+            <div>
+                text
+            </div>
+        );
+    }
+}
+
+export default BookView;
