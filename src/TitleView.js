04b06e708f59cae03e6e29fdf1fd9026af5dc248
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-18 11:17:41 -0300

    Externalising title text configuration

--- src/TitleView.js
+++ src/TitleView.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import './MyReads.css';
 
 class TitleView extends Component {
+    static propTypes = {
+        textTitle: PropTypes.string.isRequired,
+    }
 
     render() {
+        const titleText = this.props.titleText;
+
         return (
             <div className="mr-title">
-                <p className="mr-title-text">Title</p>
+                <p className="mr-title-text">{titleText}</p>
                 <div className="mr-title-line" />
             </div>
         );
