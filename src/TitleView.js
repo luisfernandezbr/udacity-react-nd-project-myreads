b40fe253b5d7374d9222ca4ad52d7b0e349ba24b
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-18 10:55:39 -0300

    Creating TitleView component

---
+++ src/TitleView.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class TitleView extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>Title</p>
+            </div>
+        );
+    }
+}
+
+export default TitleView;
