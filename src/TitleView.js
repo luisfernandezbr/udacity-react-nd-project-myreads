5eeee4cd9fd3031ebb9d91d0bb31f9a7254c6335
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-18 11:08:01 -0300

    Styling TitleView component

--- src/TitleView.js
+++ src/TitleView.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import './MyReads.css';
 
 class TitleView extends Component {
 
     render() {
         return (
-            <div>
-                <p>Title</p>
+            <div className="mr-title">
+                <p className="mr-title-text">Title</p>
+                <div className="mr-title-line" />
             </div>
         );
     }
