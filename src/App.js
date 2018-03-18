cb174b8df8a2207c842b24d705d11ef582404d93
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-18 11:45:24 -0300

    Reformating code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import TitleView from './TitleView'
 import BookView from "./BookView";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <TitleView titleText="My Title"/>
-          <BookView/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <TitleView titleText="My Title"/>
+                <BookView/>
+            </div>
+        );
+    }
 }
 
 export default App;
