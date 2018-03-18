53d8612dcfd296210adf36fda811aec4fd74faa9
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-18 11:46:22 -0300

    Externalising Book details configuration

--- src/BookView.js
+++ src/BookView.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class BookView extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    }
+
     render() {
+        const book = this.props.book;
+
         return (
             <div>
-                <img className="mr-book"/>
-                <div>Book Title</div>
-                <div>Author</div>
+                <img className="mr-book" style={{
+                    backgroundImage: `url(${book.previewUrl})`
+                }}/>
+                <div>{book.title}</div>
+                <div>{book.author}</div>
             </div>
         );
     }
