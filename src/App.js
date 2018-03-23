458ef3b710ba5bdbc6304be3530478669e8b4788
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-23 02:42:45 -0300

    Externalising Search Screen as Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import TitleView from './view/TitleView'
 import BookListView from "./view/BookListView";
+import SearchScreenView from "./view/SearchScreenView"
 import { Link, Route } from "react-router-dom"
 import * as BooksAPI from './data/BooksAPI'
 
@@ -53,31 +54,7 @@ class App extends Component {
 
     showSearchResult() {
         return (
-            <div className="search-books">
-                <div className="search-books-bar">
-                    <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
-                    <div className="search-books-input-wrapper">
-                        {/*
-                                      NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                                      You can find these search terms here:
-                                      https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                                      However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                                      you don't find a specific author or title. Every search is limited by search terms.
-                                    */
-
-                        }
-                        <input
-                            type="text"
-                            placeholder="Search by title or author"
-                            onChange={(event) => this.searchBooks(event.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className="search-books-results">
-                    <BookListView bookList={this.state.bookListReading}/>
-                </div>
-            </div>
+            <SearchScreenView/>
         );
     }
 
