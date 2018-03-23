e2d835d77a5e5a99a908459189b6a751da755e3e
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-23 00:09:42 -0300

    Refactoring code

--- src/App.js
+++ src/App.js
@@ -46,33 +46,13 @@ class App extends Component {
         )
     }
 
-    showListResult() {
-        return <div>
-            <header className="mr-header">
-                <h1 className="mr-header-title">MyReads</h1>
-            </header>
-            <div>
-                <TitleView textTitle="Currently Reading"/>
-                <BookListView bookList={this.state.bookListReading}/>
-
-                <TitleView textTitle="Want to Read"/>
-                <BookListView bookList={this.state.bookListWantToRead}/>
-
-                <TitleView textTitle="Read"/>
-                <BookListView bookList={this.state.bookListRead}/>
-            </div>
-            <div className="mr-open-search">
-                <a onClick={this.onCLickAddBook()}>Add Book</a>
-            </div>
-        </div>;
-    }
-
     showSearchResult() {
-        return <div className="search-books">
-            <div className="search-books-bar">
-                <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
-                <div className="search-books-input-wrapper">
-                    {/*
+        return (
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
+                    <div className="search-books-input-wrapper">
+                        {/*
                                       NOTES: The search from BooksAPI is limited to a particular set of search terms.
                                       You can find these search terms here:
                                       https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -81,14 +61,38 @@ class App extends Component {
                                       you don't find a specific author or title. Every search is limited by search terms.
                                     */
 
-                    }
-                    <input type="text" placeholder="Search by title or author"/>
+                        }
+                        <input type="text" placeholder="Search by title or author"/>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <BookListView bookList={this.state.bookListReading}/>
                 </div>
             </div>
-            <div className="search-books-results">
-                <BookListView bookList={this.state.bookListReading}/>
+        );
+    }
+
+    showListResult() {
+        return (
+            <div>
+                <header className="mr-header">
+                    <h1 className="mr-header-title">MyReads</h1>
+                </header>
+                <div>
+                    <TitleView textTitle="Currently Reading"/>
+                    <BookListView bookList={this.state.bookListReading}/>
+
+                    <TitleView textTitle="Want to Read"/>
+                    <BookListView bookList={this.state.bookListWantToRead}/>
+
+                    <TitleView textTitle="Read"/>
+                    <BookListView bookList={this.state.bookListRead}/>
+                </div>
+                <div className="mr-open-search">
+                    <a onClick={this.onCLickAddBook()}>Add Book</a>
+                </div>
             </div>
-        </div>;
+        );
     }
 
     onCLickAddBook() {
