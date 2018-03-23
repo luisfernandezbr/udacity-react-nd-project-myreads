4bf839bb1409ec407718f0600a3247d031bc1d4e
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-03-22 23:10:25 -0300

    Configuring header style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import TitleView from './view/TitleView'
 import BookListView from "./view/BookListView";
@@ -35,13 +34,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+                <header className="mr-header">
+                    <h1 className="mr-header-title">MyReads</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
                 <TitleView textTitle="Currently Reading"/>
                 <BookListView bookList={this.state.bookListReading}/>
 
