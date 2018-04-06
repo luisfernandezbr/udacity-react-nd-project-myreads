40e466249121fa1046e3bc048fbe6fc7c222de38
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-04-06 11:50:30 -0300

    Updating BookView component to only show content and trigger event to the parent

--- src/view/BookView.js
+++ src/view/BookView.js
@@ -1,30 +1,10 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import * as BooksAPI from '../data/BooksAPI'
 
 class BookView extends Component {
-    state = {
-        currentSelection: 'none'
-    }
-
     static propTypes = {
         book: PropTypes.object.isRequired,
-        onBookUpdated: PropTypes.func
-    }
-
-    updateBookShelf = (book, shelf) => {
-        console.log("book.title: " + book.title);
-        console.log("selected shelf: " + shelf);
-        BooksAPI.update(book, shelf)
-            .then((result) => {
-                this.setState({
-                    book: book
-                });
-                if (this.props.onBookUpdated != null) {
-                    this.props.onBookUpdated(result);
-                }
-            })
-
+        onUpdateBook: PropTypes.func
     }
 
     render() {
@@ -40,7 +20,7 @@ class BookView extends Component {
 
                     <div className="book-shelf-changer">
                         <select defaultValue={book.shelf} onChange={event => {
-                            this.updateBookShelf(book, event.target.value)
+                            this.props.onUpdateBook(book, event.target.value)
                         }}>
                             <option value="none" disabled>Move to...</option>
                             <option value="currentlyReading">Currently Reading</option>
