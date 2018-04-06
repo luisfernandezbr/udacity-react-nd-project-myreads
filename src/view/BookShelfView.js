73ae68c16bdf2ed5ba2aa840a586126fe42a2fd0
Luis Fernandez <luis.fernandez@mobiplus.com.br>
2018-04-06 11:48:04 -0300

    Creating Shelf component to handle correct different shelves

---
+++ src/view/BookShelfView.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import TitleView from "./TitleView";
+import BookListView from "./BookListView";
+
+class BookShelfView extends Component {
+    static propTypes = {
+        shelfTitle: PropTypes.string.isRequired,
+        shelfKey: PropTypes.string.isRequired,
+        bookList: PropTypes.array.isRequired,
+        onUpdateBook: PropTypes.func.isRequired
+    }
+
+    render() {
+        return (
+            <div>
+                <TitleView textTitle={this.props.shelfTitle}/>
+                <BookListView
+                    bookList={this.props.bookList}
+                    onUpdateBook={this.props.onUpdateBook.bind(this)}
+                />
+            </div>
+        );
+    }
+}
+
+export default BookShelfView
